de8eeb73a9e08181b572beb1958119903fd3e9f7
rkolisetty <rkolisetty@processdisplays.com>
2020-08-13 20:38:11 +0530

    code

---
+++ EventHubTest/BlobStorage.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHubTest
+{
+    public class BlobStorage
+    {
+
+        public static void SavePdf(string blobConnection)
+        {
+            //upload file blob azure storage
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse((blobConnection));
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("BlobStorageContainername");
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference("JNCASR APPLICATION FORM");
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string filePath = System.IO.Path.Combine(currentDirectory, "Documents", "JNCASR APPLICATION FORM.pdf");
+            using (var fileStream = System.IO.File.OpenRead(filePath))
+            {
+                blockBlob.UploadFromStream(fileStream);
+            }
+        }
+    }
+    
+}
---
+++ EventHubTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using EventHubTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string EventHubConnectionString = ConfigurationManager.ConnectionStrings["EventHubConnectionString"].ConnectionString;
+            string BlobStorageConnection = ConfigurationManager.ConnectionStrings["BlobStorageConnection"].ConnectionString;
+            BlobStorage.SavePdf(BlobStorageConnection);
+            AttachmentMetaData eventHubMessage = new AttachmentMetaData()
+            {
+               
+            };
+            EventHubsHelper.PushMessageToEventHubsAsync(eventHubMessage, EventHubConnectionString);
+
+        }
+    }
+}
